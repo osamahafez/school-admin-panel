640dda4d3efb7804d756b6b1cba2e964ede900af
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-24 23:32:17 +0200

    add delete request for the students route

--- routes/api/students.js
+++ routes/api/students.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const Student = require('../../models/Student');
 const validateStudentInputs = require('../../validation/students');
 const passport = require('passport');
+// const IsEmpty = require('../../validation/IsEmpty');
 
 /*
+@req: post
 @route: /api/students/create
 @description: create a new student
 @access: private
@@ -24,7 +26,7 @@ router.post('/create', passport.authenticate('jwt', {session: false}), (req, res
     studentData.stage = req.body.stage;
     studentData.level = req.body.level;
     studentData.parent_info = req.body.parent_info;
-    
+
     const student = new Student(studentData);
     student.save()
         .then(() => res.status(201).json(student))
@@ -33,15 +35,28 @@ router.post('/create', passport.authenticate('jwt', {session: false}), (req, res
 });
 
 /*
+@req: get
 @route: /api/students/:student_id
 @description: find student by id
 @access: private
 */
-router.get('/:student_id'. passport.authenticate('jwt', {session:false}), (req, res) => {
-
+router.get('/:student_id', passport.authenticate('jwt', {session:false}), (req, res) => {
     Student.findById(req.params.student_id)
         .then(result => res.status(200).json(result))
         .catch(() => res.status(404).json({notFound: 'Student doesn\'t exist'}));
+});
+
+
+/*
+@req: delete
+@route: /api/students/:student_id
+@description: delete student by id
+@access: private
+*/
+router.delete('/:student_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Student.findByIdAndDelete(req.params.student_id)
+        .then(() => res.status(200).json({msg: 'Student deleted successfully'}))
+        .catch(err => res.status(400).json(err));
 });
 
 
