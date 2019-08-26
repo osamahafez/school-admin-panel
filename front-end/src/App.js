7a50111d3a28a4b33e50b97b8fb998a4b375aeec
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-26 22:22:54 +0200

    adding front end

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='display-4'>Admin Panel</h1>
+    </div>
+  );
+}
+
+export default App;
