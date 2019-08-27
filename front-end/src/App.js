304b8b3a2d6f4720850505d0b20bd3e78407423e
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-27 14:58:53 +0200

    setup react-router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import AdminLogin from './components/AdminLogin/AdminLogin'
+import SideBar from './components/SideBar/SideBar';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='display-4'>Admin Panel</h1>
-    </div>
-  );
+    return (
+        <div className='App'>
+           <Route exact path='/' component={AdminLogin} />
+        </div>
+    );
 }
 
 export default App;
